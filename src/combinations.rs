b08a23a557f9d39be73fca054083a798e16d6595
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-12-07 19:53:29 +0100

    some micro-optimizations

--- src/combinations.rs
+++ src/combinations.rs
@@ -44,27 +44,64 @@ where I: Iterator, I: Clone, I::Item: Clone {
         if self.items.is_empty() {
             self.finished = true;
         } else {
-            'outer: for index in (1..self.items.len()).rev() {
-                // TODO: optimize clones!
+            /*
+             * The following could be just this, but that has some unnecessary
+             * clones of the iterator and checks the index == 0 potentially
+             * multiple times.
+
+            'outer: for index in (0..self.items.len()).rev() {
                 let mut iter = self.items[index].0.clone();
 
                 for tail_index in index..self.items.len() {
+                    let Some(value) = iter.next() else {
+                        if index == 0 {
+                            self.finished = true;
+                            break;
+                        }
+                        continue 'outer;
+                    };
+                    self.items[tail_index] = (iter.clone(), value);
+                }
+
+                break;
+            }
+             */
+
+            'outer: for index in (1..self.items.len()).rev() {
+                let mut iter = self.items[index].0.clone();
+
+                for tail_index in index..(self.items.len() - 1) {
                     let Some(value) = iter.next() else {
                         continue 'outer;
                     };
                     self.items[tail_index] = (iter.clone(), value);
                 }
+
+                if let Some(value) = iter.next() {
+                    // eliminate one unnecessary clone
+                    *self.items.last_mut().unwrap() = (iter, value);
+                } else {
+                    continue 'outer;
+                }
+
                 return Some(res);
             }
 
             let mut iter = self.items[0].0.clone();
 
-            for tail_index in 0..self.items.len() {
+            for tail_index in 0..(self.items.len() - 1) {
                 let Some(value) = iter.next() else {
                     self.finished = true;
                     break;
                 };
                 self.items[tail_index] = (iter.clone(), value);
+            }
+
+            if let Some(value) = iter.next() {
+                // eliminate one unnecessary clone
+                *self.items.last_mut().unwrap() = (iter, value);
+            } else {
+                self.finished = true;
             }
         }
 
